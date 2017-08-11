c2eeb17528295200304ecf44ee8405a859c2b96a
_meugen_ <eugen35@mail.ru>
2017-08-11 10:00:10 +0500

    Небольшой рефакторинг seedDb

--- data/seedDb.js
+++ data/seedDb.js
@@ -9,41 +9,45 @@ export default function seedDb(db) {
   const { Observation, Unit, Person } = db.models;
   db.sync({ force: true }).then(() => {
     Promise.all([seedUnits(Unit), seedPersons(Person)]).then(([units, persons]) => {
-      _.times(10, () => {
-        return Observation.create({
-          evidence: casual.sentences(2),
-          date: casual.date('YYYY-MM-DD'),
-          requirement: JSON.stringify({
-            normativeDocument: casual.random_element([
-              'ISO 9001:2015', 'ISO 14001:2015', 'OHSAS 18001:2007'
-            ]),
-            clause: casual.random_element([
-              '4.1', '4.2', '5.1', '5.2', '5.3', '6.1', '7.1', '7.2', '7.3', '8.1', '9.1', '10.1'
-            ]),
-            quote: casual.sentences(2),
-          }),
-          type: casual.random_element(ObservationType),
-          // date: casual.date,
-          status: casual.random_element(ObservationStatus),
-        }).then((observation) => {
-          observation.setUnit(casual.random_element(units));
-          observation.setPerson(casual.random_element(persons));
-        });
-      });
+      seedObservations({ Observation, units, persons });
     });
   });
 }
 
-// @returns {Promise} - промис на создание в БД unita
+// @returns {Promise[]} - промис на массив созданных в БД unitов
 function seedUnits(Unit) {
   return Promise.all(UnitNames.map((unitName) => Unit.create({ name: unitName })));
 }
-// @returns {Promise} - промис на создание в БД unita
+// @returns {Promise[]} - промис на массив созданных в БД персонс
 function seedPersons(Person) {
   return Promise.all(_.times(7, () => Person.create({
     firstName: casual.first_name,
     secondName: casual.last_name,
   })));
+}
+
+// @returns {Promise[]} - промис на массив созданных в БД observations. Которые уже связаны с unit и person
+function seedObservations({ Observation, units, persons }){
+  return Promise.all(_.times(10, () => Observation.create({
+    evidence: casual.sentences(2),
+    date: casual.date('YYYY-MM-DD'),
+    requirement: JSON.stringify({
+      normativeDocument: casual.random_element([
+        'ISO 9001:2015', 'ISO 14001:2015', 'OHSAS 18001:2007'
+      ]),
+      clause: casual.random_element([
+        '4.1', '4.2', '5.1', '5.2', '5.3', '6.1', '7.1', '7.2', '7.3', '8.1', '9.1', '10.1'
+      ]),
+      quote: casual.sentences(2),
+    }),
+    type: casual.random_element(ObservationType),
+    // date: casual.date,
+    status: casual.random_element(ObservationStatus),
+  }).then((observation) => {
+    observation.setUnit(casual.random_element(units));
+    observation.setPerson(casual.random_element(persons));
+  })
+  ));
 }
 
 const ObservationType = [
